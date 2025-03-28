c3a6978d071bb3cb026b024d3c564dd8560cede0
Viktor <1180505n@gmail.com>
2025-03-28 23:11:24 +0100

    initial commit

---
+++ src/main/java/de/ait/javalessons/clinicproject/ClinicProjectApplication.java
@@ -0,0 +1,13 @@
+package de.ait.javalessons.clinicproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/ait/javalessons/clinicproject/ClinicProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ait.javalessons.clinicproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
